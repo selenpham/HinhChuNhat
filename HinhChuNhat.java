7d44227d002f34e52f8877820324bb568a72c0e7
Hai Thanh Pham <59055154+selenpham@users.noreply.github.com>
2023-04-19 09:56:14 +0700

    Add files via upload

--- HinhChuNhat.java
+++ HinhChuNhat.java
@@ -8,12 +8,12 @@ public class HinhChuNhat {
 	
 	public int dai;
 	public int rong;
-		
+//Tạo công thức tính chu vi	
 	public int tinhChuvi() {
 		int chuvi = (this.dai+this.rong)*2;
 		return chuvi;
 	}
-	
+	//Tạo công thức tính Diện tích		
 	public int tinhDientich() {
 		int dientich = this.dai*this.rong;
 		return dientich;
@@ -22,20 +22,36 @@ public class HinhChuNhat {
 	public static void main(String[] args) {
 		HinhChuNhat o = new HinhChuNhat();
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Nhap vao chieu dai: ");
-		int chieudai = scanner.nextInt();
-		o.dai = chieudai;
-		
-		System.out.print("Nhap vao chieu rong: ");
-		int chieurong = scanner.nextInt();
-		o.rong = chieurong;
-		
-		int chuvi = o.tinhChuvi();
-		int dientich = o.tinhDientich();
-		System.out.println("Chu vi hinh chu nhat = "+ chuvi);
-		System.out.println("Dien tich hinh chu nhat = "+ dientich);
+		boolean exit = false;
+// sử dụng vòng lặp nhập thông số, khi người dùng nhấn E thì thoát		
+		while (!exit) {
+			try {
+				System.out.print("Nhap vao chieu dai (hoac nhap E de thoat): ");
+				String daiInput = scanner.nextLine();
+				
+				if (daiInput.equalsIgnoreCase("E")) {
+					exit = true;
+					break;
+				}
+				
+				o.dai = Integer.parseInt(daiInput);
+				
+				System.out.print("Nhap vao chieu rong: ");
+				o.rong = Integer.parseInt(scanner.nextLine());
 
+				int chuvi = o.tinhChuvi();
+				int dientich = o.tinhDientich();
+				System.out.println("Chu vi hinh chu nhat = "+ chuvi);
+				System.out.println("Dien tich hinh chu nhat = "+ dientich);
+				
+			} catch (NumberFormatException ex) {
+				System.out.println("Vui long nhap gia tri so hoac E de thoat!");
+			}
+		}
+		
+		System.out.println("Chuong trinh da thoat.");
 		
 	}
+
 
 }
